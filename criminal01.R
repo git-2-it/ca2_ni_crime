77999f3ea1bc09765c099d8c9e32df017289de3c
git-2-it <59895978+git-2-it@users.noreply.github.com>
2020-04-07 18:18:56 +0100

    comm 02

--- criminal01.R
+++ criminal01.R
@@ -4,7 +4,6 @@
 # Read data file from data directory
 # no header, defined seperator, blanks set as NA
 postcodes_in <- read.csv("data/NIPostcodes.csv", header=FALSE, sep=",")
-#postcodes_in <- read.csv("data/NIPostcodes.csv", header=FALSE, sep=",", na = c(" ",""))
 
 # show structure
 str(postcodes_in)
@@ -26,49 +25,100 @@ str(postcodes_in)
 # y-coordinates
 # Primary Key (identifier)
 
-# Build column names
+# Build and assign column names replacing spaces with underscores to make field handing easier
 column_names <- c("Organisation","Sub-building", "Building", "Number",
-                  "Primary Thorfare", "Alt Thorfare", "Secondary Thorfare",
+                  "Primary_Thorfare", "Alt_Thorfare", "Secondary_Thorfare",
                   "Locality", "Townland", "Town","County","Postcode",
-                  "x-coordinates", "y-coordinates","Primary Key")
+                  "x-coordinates", "y-coordinates","Primary_Key")
 
 colnames(postcodes_in) <- column_names
 
 # Check named columns
 str(postcodes_in)
 
-# What is the number of raw rows
+# Confirm  is the number of raw rows (943 034)
 nrow(postcodes_in)
 
 # First 10 complete rows
 # (really complete, na on all blanks is 0?)
 head(postcodes_in[complete.cases(postcodes_in),], n=10)
 
-# Begin to deal with missing data, filtering what is missing and necessary, vs missing and unnecessary
+# Begin to deal with missing data, filtering what is missing and necessary, 
+# vs missing and unnecessary
+# This will help decide how to manage blanks values in the dataset
 str(postcodes_in)
 
-# Based on reasonable assumption the fields can be seperated into required and non-required fields
-# eg this postcode data, therefore  missing postcode is a problem
-# Required  -
-# Key
-# postcode
-# x and y- coordinates
-# Country
-# Town
-# Primary Thorfare
+# Based on reasonable assumptions the fields can be seperated into required and non-required fields
+# eg this postcode data, therefore  missing postcode is a problem, organisation is not so relevant
+# First, run through the data to see where a choice should be made
 
 # No empty values - 
+# Primary Key
+# x- and y- coordinates
 # townland
+# County
 
 # Not strictly required -
 # Organsiation
+# Building
 # sub-building
-# Primary Thorfare
+# Locality
+# Secondary Thorfare
 # Alt Thorfare
-# Number
+# Number (not all addresses have a street number)
+
+# Possilbly required  -
+# Town
+# postcode
+# Primary Thorfare
+
+sub_post <- postcodes_in[ c(5,10,12,15) ]
+
+str(sub_post)
 
 
-# Check numbers of records with required data missing
+# Check a summary view of the data, noting total records count is 943 034
+summary(sub_post)
+
+# On checking if there are records with required data missing, ie blank/empty factor values
+# postcode - empty values
+# Town - empty values
+# Primary Thorfare - empty values
+
+# Town is arguably categorical data, categorical data should not be removed
+# Check on number of potentially affected records none the less
+# set the blanks to NA and check the resulting NA count
+sub_post$Town[sub_post$Town == ""] <- NA
+countNA(sub_post$Town)
+
+# Approx 20k missing town records is substantial, missing > 20%
+# Therefore  leave it in place
+
+# Checking on  Postcode
+# Summary shows 8 900 blanks out of total, missing < 1%
+# Therefore they could be removed
+# If, in later analysis, the issing values were important, its possible 
+# the x/y co-ordinatres could be used to guess the missing values
+
+# Check on Primary_Thorfare
+# set the blanks to NA and check the resulting NA count
+sub_post$Primary_Thorfare[sub_post$Primary_Thorfare == ""] <- NA
+countNA(sub_post$Primary_Thorfare)
+
+# approx 470 missing Primary_Thorfare records not substantial, missing < 0.1%
+
+# Therefore, it should be possible to remove blank Primary_Thorfare and Postcode
+
+
+library(mice)
+md.pattern(sub_post)
+
+library(VIM)
+missing_values <- aggr(sub_post, prop = FALSE, numbers = TRUE)
+# Show summary of the contents of missing_values
+summary(missing_values)
+
+str(postcodes_in)
 
 
 
