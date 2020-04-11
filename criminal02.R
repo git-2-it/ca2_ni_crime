80b493d648982f8fa60a581e6821f4cfff5a587b
git-2-it <59895978+git-2-it@users.noreply.github.com>
2020-04-11 14:19:04 +0100

    Commit 12

--- criminal02.R
+++ criminal02.R
@@ -8,6 +8,7 @@ crimes_in <- read.csv("data/NI Crime Data/2015-01/2015-01-northern-ireland-stree
 # dataset review
 str(crimes_in)
 
+
 # Remove specified columns
 crimes_in <- subset(crimes_in, select = -c(Crime.ID, 
                               Reported.by, Falls.within, LSOA.code, LSOA.name, 
@@ -17,8 +18,6 @@ crimes_in <- subset(crimes_in, select = -c(Crime.ID,
 str(crimes_in)
 
 # Abbreviate crime types 
-library(dplyr)
-
 # Import dplyr library for manipulation functions
 library(dplyr)
 
@@ -49,7 +48,6 @@ levels(crimes_in$Crime.type)
 
 # All input crime factor descriptions now shortened
 
-
 # Data reduced and tidyied
 # Now to move to plotting the data
 
@@ -61,7 +59,6 @@ before_plt <- par()
 library("viridis")
 
 plot_table <- table(crimes_in$Crime.type)
-# plot(crimes_in)
 str(plot_table)
 
 barplot(plot_table, 
@@ -80,7 +77,7 @@ barplot(plot_table,
 crimes_in$Location <- gsub( "On or near", "", crimes_in$Location)
 crimes_in$Location <- trimws(crimes_in$Location)
 
-# View small sample of the data
+# View small sample of the data to confirm the removal
 sample_n(crimes_in, 20)
 
 # Set empty locations to NA
@@ -90,24 +87,28 @@ crimes_in$Location[crimes_in$Location == ""] <- NA
 set.seed(100)
 random_crime_sample <- sample_n(na.omit(crimes_in), 5000, replace = FALSE)
 
-samp <- sample_n(random_crime_sample, 10, replace = TRUE)
-
-
-#Re-read Postcode data from previously
+# Re-read Postcode data created previously
 postcodes_clean <- read.csv("data/CleanNIPostcodeData.csv", header=TRUE, sep=",")
-
-# Indexing postcode data by street name, ie primary thouroughfare
 
 # Create function find_a_town
 # function is to find, for an individual entry, a value for town based on location
+
 library("data.table")
+# Create data table
 postcodes_table <- data.table(postcodes_clean)
 
-# Create the function
-# Removes blank town values and returns only the first entry, obviously leads to possibly incorrect
-# data, eg consider case of "man street" which is in partically every town in the country
+# Indexing postcode data by street name, ie primary thouroughfare
+setkey(postcodes_table, Primary_Thorfare)
+
+# Now create the function
 find_a_town <- function(row_in)
 {
+  # Removes blank town values and returns only the first entry, 
+  # Naive function, could lead to possibly incorrect
+  # data, eg consider case of "man street" which is in pratically 
+  # every town in the country
+  # Expects data row containing a Location field as input
+  
   loc_in <- row_in[c("Location")]
 
   # Tidy function input eg remove case sensitivity
@@ -125,33 +126,44 @@ find_a_town <- function(row_in)
 }
 
 # Add new town column to dataset
-samp$Town <- apply(samp, 1, find_a_town)
+# samp$Town <- apply(samp, 1, find_a_town)
+random_crime_sample$Town <- apply(random_crime_sample, 1, find_a_town)
 
 
-# Read in villages data containing population data
+# Read in villages data containing population figures
 villages <- read.csv("data/VillageList.csv", header=TRUE, sep=",")
+str(villages)
 
+# Change odd looking column name and set values to uppercase for convienience
 colnames(villages)[1] <- "CITY.TOWN.VILLAGE"
 villages$CITY.TOWN.VILLAGE <- toupper(villages$CITY.TOWN.VILLAGE)
 
-
-str(villages)
+# Create a data.table for search
 villages_table <- data.table(villages)
 
+# Set an index in data table
+setkey(villages_table, CITY.TOWN.VILLAGE)
+
+# View new table
 str(villages_table)
+
 
 # Lookup population function
 add_town_data <- function(row_in)
 {
+  # Expects data row containing a Location field as input
+  # Retuns either a value or an NA if bad input or data not found
+  
   loc_in <- row_in[c("Town")]
   
-  cat(loc_in)
+  retVal <- NA
+  
   if (is.na(loc_in) | loc_in == "")
   {
-    return(NA)
+    return(retVal)
   }
 
-  # Deal with Derry  
+  # Deal with Derry naming
   if (loc_in == "LONDONDERRY" )
   {
      loc_in <- "DERRY"
@@ -170,17 +182,50 @@ add_town_data <- function(row_in)
   return(retVal[1])
 }
 
-str(villages)
+random_crime_sample$Population <- apply(random_crime_sample, 1, add_town_data)
 
-samp <- sample_n(na.omit(random_crime_sample), 10, replace = TRUE)
-samp$Town <- apply(samp, 1, find_a_town)
-str(samp)
 
-#samp$Town <- 
-samp$Population <- apply(samp, 1, add_town_data)
 
-add_town_data("Beflast")
+# Add town population data to the sample 
+random_crime_sample$Population <- apply(random_crime_sample, 1, add_town_data)
 
-str(samp)
+# Save sample dataset file
+write.csv(file="data/random_crime_sample.csv", x=random_crime_sample, quote=TRUE, row.names = FALSE)
 
-structure(samp)
+
+# Charting data figures for Belfast and Derry
+
+# Extract belfast/Derry data 
+# First, clean up the NA Town values
+random_crime_sample <- na.omit(random_crime_sample)
+
+# Need to add new level in factors for Derry  before can assign it
+levels(random_crime_sample$Town) <- c(levels(random_crime_sample$Town), "DERRY")
+random_crime_sample$Town[random_crime_sample$Town == "LONDONDERRY"] <- "DERRY"
+
+# Filter based on Town Values
+belderr <- random_crime_sample %>% filter(Town == "BELFAST" | Town == "DERRY")
+str(belderr)
+
+# Drop unnecessary factors levels to make plotting etc simpler
+belderr <- droplevels(belderr)
+
+# Store the current  par setup before next set of visuals
+init_par <- par()
+
+# Create table of data for plots
+plot_table <- table(belderr$Town, belderr$Crime.type, exclude = TRUE)
+
+str(plot_table)
+
+# Create side-by-side bar chart to allow comparison between the two cities
+barplot(plot_table, 
+        main = "NI Crime Statistics: Belfast and Derry",
+        xlab = "Crime Type", 
+        ylab = "Frequency",
+        col = viridis(2),
+        cex.names = 0.75,
+        legend = TRUE , 
+        beside = TRUE
+)
+
